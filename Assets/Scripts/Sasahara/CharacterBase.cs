97038ef62f5f5200c577440cf1da392190973c0d
S-Hiro <hiroto-_-1610@docomo.ne.jp>
2023-06-26 18:02:19 +0900

    [add]キャラクターベースクラスの作成

---
+++ Assets/Scripts/Sasahara/CharacterBase.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CharactorBase : MonoBehaviour
+{
+    #region property
+    // プロパティを入れる。
+    public int _hp;
+    public int _power;
+
+    #endregion
+
+    #region serialize
+    // unity inpectorに表示したいものを記述。
+    #endregion
+
+    #region private
+    // プライベートなメンバー変数。
+    #endregion
+
+    #region Constant
+    // 定数をいれる。
+    #endregion
+
+    #region Event
+    //  System.Action, System.Func などのデリゲートやコールバック関数をいれるところ。
+    #endregion
+
+    #region unity methods
+    //  Start, UpdateなどのUnityのイベント関数。
+    private void Awake()
+    {
+
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+    #endregion
+
+    #region public method
+    //　自身で作成したPublicな関数を入れる。
+    public virtual void Attack(int power)
+    {
+        Debug.Log("Attack");
+    }
+
+    public virtual void Damaged(int power)
+    {
+        Debug.Log("Damaged");
+    }
+    #endregion
+
+    #region private method
+    // 自身で作成したPrivateな関数を入れる。
+    #endregion
+}
