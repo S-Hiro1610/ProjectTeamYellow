92c0283ad2396172693a31af244ffa3fb0af09f4
kannkeii <kannkeii@outlook.jp>
2023-06-27 00:46:37 +0900

    [add]InputManager.csとUIManager.csの処理追加。 ・InputManager.csにマウス左クリックとOneタッチの探知機能を追加。 ・UIManager.csにテストスコアを表示させるテスト関数を追加。

--- Assets/Scripts/Han/UIManager.cs
+++ Assets/Scripts/Han/UIManager.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     #region property
     // プロパティを入れる。
+    public static UIManager Instance;
+
+    public Text testScoreText;
     #endregion
 
     #region serialize
@@ -14,6 +18,7 @@ public class UIManager : MonoBehaviour
 
     #region private
     // プライベートなメンバー変数。
+    private int testScore;
     #endregion
 
     #region Constant
@@ -28,7 +33,13 @@ public class UIManager : MonoBehaviour
     //  Start, UpdateなどのUnityのイベント関数。
     private void Awake()
     {
-
+        if(Instance == null)
+        {
+            Instance = this;
+        }else
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void Start()
@@ -44,6 +55,24 @@ public class UIManager : MonoBehaviour
 
     #region public method
     //　自身で作成したPublicな関数を入れる。
+    /*AddTestScore(int points)使用する場合:
+    public class PlayerController : MonoBehaviour
+    {
+        void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.CompareTag("Enemy"))
+            {
+                UIManager.Instance.AddTestScore(100);
+                Destroy(collision.gameObject);
+            }
+        }
+    }
+     */
+    public void AddTestScore(int points)
+    {
+        testScore += points;
+        testScoreText.text = "Score: " + testScore;
+    }
     #endregion
 
     #region private method
