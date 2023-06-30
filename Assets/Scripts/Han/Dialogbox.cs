333a32bb960f9e5c86f66c6d8b90c1f38b94312c
kannkeii <kannkeii@outlook.jp>
2023-06-30 17:11:53 +0900

    [update]Dialogbox.csコードを綺麗に修正。Exitダイヤログのマスクサイズを修正。

--- Assets/Scripts/Han/Dialogbox.cs
+++ Assets/Scripts/Han/Dialogbox.cs
@@ -6,27 +6,55 @@ using UnityEngine.UI;
 
 public class Dialogbox : MonoBehaviour
 {
+    #region property
+    // プロパティを入れる。
     public Button ExitButton;
 
     public UnityEvent OnOpenEvent;
     public UnityEvent OnCloseEvent;
-    // Start is called before the first frame update
-    void Start()
+    #endregion
+
+    #region serialize
+    // unity inpectorに表示したいものを記述。
+    #endregion
+
+    #region private
+    // プライベートなメンバー変数。
+    #endregion
+
+    #region Constant
+    // 定数をいれる。
+    #endregion
+
+    #region Event
+    //  System.Action, System.Func などのデリゲートやコールバック関数をいれるところ。
+    #endregion
+
+    #region unity methods
+    //  Start, UpdateなどのUnityのイベント関数。
+    private void Awake()
     {
         ExitButton.onClick.AddListener(() => { SetActive(false); });
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+
     }
 
+    private void Update()
+    {
+
+    }
+    #endregion
+
+    #region public method
+    //　自身で作成したPublicな関数を入れる。
     public void SetActive(bool onoff)
     {
         //gameObject.transform.parent.gameObject.SetActive(onoff);
 
-        if(onoff)
+        if (onoff)
         {
             Open();
             return;
@@ -47,4 +75,9 @@ public class Dialogbox : MonoBehaviour
         gameObject.transform.parent.gameObject.SetActive(false);
         OnCloseEvent.Invoke();
     }
+    #endregion
+
+    #region private method
+    // 自身で作成したPrivateな関数を入れる。
+    #endregion
 }
