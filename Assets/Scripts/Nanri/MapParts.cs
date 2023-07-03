b560c0a79181ef3bfb8675c14eb17cec64feb0ea
TomoNanri <131641585+TomoNanri@users.noreply.github.com>
2023-07-03 15:40:27 +0900

    [add] Editor/MapPartsフォルダ追加 [add] Editor/MapPartsフォルダ以下にデバッグ用のパーツプレファブを作成 [add] Editor/MapParts/Texture2Dフォルダ追加 [add] デバッグ用Texture2D画像を追加 [update] MapEditorMainにマップパーツ選択ボタンを追加 [update] 画面コンポーネント配置をScopeにまとめた

---
+++ Assets/Scripts/Nanri/MapParts.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// マップエディタデバッグ用
+/// このオブジェクトに Texture2D をアタッチして
+/// ツールバーに表示させる
+/// </summary>
+
+public class MapParts : MonoBehaviour
+{
+    #region property
+    public Texture2D MapTexture => _mapTexture;
+    #endregion
+
+    #region private
+    [SerializeField]
+    Texture2D _mapTexture;
+    #endregion
+
+    #region unity methods
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    #endregion
+}
