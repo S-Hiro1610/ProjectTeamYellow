83d214ef26eb578e4d9c8a28a9d97083c9d83e3c
S-Hiro <hiroto-_-1610@docomo.ne.jp>
2023-07-12 16:09:20 +0900

    [add]ゲームサイクルを停止させるイベント、およびゲームステート用のイベントを作成

--- Assets/Scripts/Sasahara/System/GameManager.cs
+++ Assets/Scripts/Sasahara/System/GameManager.cs
@@ -1,13 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
+using System;
 
 public class GameManager : MonoBehaviour
 {
     #region property
     // プロパティを入れる。
-    public GameState CurrentGameState => _currentGameState;
     public static GameManager Instance => _instance;
+    public ReactiveProperty<int> Resouce => _resouce;
+    public ReactiveProperty<int> EnemyCount => _enemyCount;
+    public GameState CurrentState => _currentState;
+    public IObservable<Unit> OnStop => _stopEvent;
+    public IObservable<Unit> OnStart => _startEvent;
+    public IObservable<Unit> OnChangeTitle => _changeTitleEvent;
+    public IObservable<Unit> OnChangeInitialize => _changeInitializeEvent;
+    public IObservable<Unit> OnChangeInGame => _changeInGameEvent;
+    public IObservable<Unit> OnChangeGameOver => _changeGameOverEvent;
     #endregion
 
     #region serialize
@@ -16,8 +26,25 @@ public class GameManager : MonoBehaviour
 
     #region private
     // プライベートなメンバー変数。
+    /// <summary>1秒間に加算されるリソースの数</summary>
     [SerializeField]
-    private GameState _currentGameState = GameState.Title;
+    private int _addResouce = 100;
+
+    /// <summary>手持ちの配置リソース</summary>
+    private ReactiveProperty<int> _resouce = new ReactiveProperty<int>(0);
+
+    /// <summary>倒した敵のカウント</summary>
+    private ReactiveProperty<int> _enemyCount = new ReactiveProperty<int>(0);
+
+    private Subject<Unit> _stopEvent = new Subject<Unit>();
+    private Subject<Unit> _startEvent = new Subject<Unit>();
+    private Subject<Unit> _changeTitleEvent = new Subject<Unit>();
+    private Subject<Unit> _changeInitializeEvent = new Subject<Unit>();
+    private Subject<Unit> _changeInGameEvent = new Subject<Unit>();
+    private Subject<Unit> _changeGameOverEvent = new Subject<Unit>();
+
+    private GameState _currentState = GameState.Title;
+    private bool _isPlay = false;
     private static GameManager _instance;
     #endregion
 
@@ -38,10 +65,18 @@ public class GameManager : MonoBehaviour
             _instance = this;
             return;
         }
-        else if(_instance != this)
+        else if (_instance != this)
         {
             Destroy(gameObject);
         }
+
+        _stopEvent.Subscribe(_ => ChangePlayFrag());
+        _startEvent.Subscribe(_ => ChangePlayFrag());
+
+        _changeTitleEvent.Subscribe(_ => SetCurrentState(GameState.Title));
+        _changeInitializeEvent.Subscribe(_ => SetCurrentState(GameState.Initialize));
+        _changeInGameEvent.Subscribe(_ => SetCurrentState(GameState.InGame));
+        _changeGameOverEvent.Subscribe(_ => SetCurrentState(GameState.GameOver));
     }
 
     private void Start()
@@ -57,21 +92,55 @@ public class GameManager : MonoBehaviour
 
     #region public method
     //　自身で作成したPublicな関数を入れる。
-    public void SetGameState(GameState state)
+    /// <summary>倒した敵をカウントする</summary>
+    public void AddEnemyCount()
     {
-        _currentGameState = state;
+        _enemyCount.Value++;
+        //UI側で EnemyCount.Subscribe(x => [ここに表示を更新する処理(xが値)])
+    }
+
+    public void TimerStop()
+    {
+        _stopEvent.OnNext(Unit.Default);
+    }
+
+    public void TimerStart()
+    {
+        _startEvent.OnNext(Unit.Default);
     }
     #endregion
 
     #region private method
     // 自身で作成したPrivateな関数を入れる。
+    private IEnumerator AddResouce()
+    {
+        while (true)
+        {
+            if (_isPlay)
+            {
+                yield return new WaitForSeconds(1);
+                _resouce.Value += _addResouce;
+                //UI側で Resouce.Subscribe(x => [ここに表示を更新する処理(xが値)])
+            }
+        }
+    }
+
+    private void ChangePlayFrag()
+    {
+        _isPlay = !_isPlay;
+    }
+
+    private void SetCurrentState(GameState state)
+    {
+        _currentState = state;
+    }
     #endregion
 }
-public enum GameState 
+
+public enum GameState
 {
     Title,
-    Stage1,
-    Stage2
-    //ステージ追加によって追加で記述
+    Initialize,
+    InGame,
+    GameOver
 }
-
